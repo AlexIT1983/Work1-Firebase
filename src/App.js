4422a0488e3844f80d1189a135c9ad5d24472ade
Alex Antipov <alex.antipov@gmail.com>
2024-02-20 20:10:21 +0300

    Work1 variant 2 JSON Server

--- src/App.js
+++ src/App.js
@@ -1,42 +1,108 @@
-// Задание 1.  (вариант 1) (5. Запросы к серверу)
-// JSON Placeholder
+// Задание 1.  (вариант 2) (5. Запросы к серверу)
+// JSON Server
 
 import { useEffect, useState } from "react";
+import { Todo, ControlPanel } from "./components";
+import { createTodo, readTodos, updateTodo, deleteTodo } from "./api";
+import {
+	addTodoInTodos,
+	findTodo,
+	removeTodoInTodos,
+	setTodoInTodos,
+} from "./utils";
+import { NEW_TODO_ID } from "./constants";
 import styles from "./App.module.css";
 
 export const App = () => {
 	const [todos, setTodos] = useState([]);
 	const [isLoading, setIsLoading] = useState(false);
+	const [searchPhrase, setSearchPhrase] = useState("");
+	const [isAlphabetSorting, setIsAlphabetSorting] = useState(false);
+
+	// добавление задачи
+	const onTodoAdd = () => {
+		setTodos(addTodoInTodos(todos));
+	};
+
+	// функция сохранения
+	const onTodoSave = (todoId) => {
+		const { title, completed } = findTodo(todos, todoId) || {};
+		if (todoId === NEW_TODO_ID) {
+			createTodo({ title, completed }).then((todo) => {
+				let updatedTodos = setTodoInTodos(todos, {
+					id: NEW_TODO_ID,
+					isEditing: false,
+				});
+				updatedTodos = removeTodoInTodos(updatedTodos, NEW_TODO_ID);
+				updatedTodos = addTodoInTodos(updatedTodos, todo);
+				setTodos(updatedTodos);
+			});
+		} else {
+			updateTodo({ id: todoId, title }).then(() => {
+				setTodos(
+					setTodoInTodos(todos, { id: todoId, isEditing: false }),
+				);
+			});
+		}
+	};
+
+	const onTodoEdit = (id) => {
+		setTodos(setTodoInTodos(todos, { id, isEditing: true }));
+	};
+
+	const onTodoCompletedChange = (id, newCompleted) => {
+		updateTodo({ id, completed: newCompleted }).then(() => {
+			setTodos(setTodoInTodos(todos, { id, completed: newCompleted }));
+		});
+	};
+
+	const onTodoRemove = (id) => {
+		// функция удаления
+		deleteTodo(id).then(() => setTodos(removeTodoInTodos(todos, id)));
+	};
+	const onTodoTitleChange = (id, newTitle) => {
+		setTodos(setTodoInTodos(todos, { id, title: newTitle }));
+	};
 
 	useEffect(() => {
 		setIsLoading(true);
 
-		fetch("https://jsonplaceholder.typicode.com/todos")
-			.then((loadedData) => loadedData.json())
-			.then((loadedTodos) => {
-				setTodos(loadedTodos);
-			})
+		readTodos(searchPhrase, isAlphabetSorting)
+			.then((loadedTodos) => setTodos(loadedTodos))
 			.finally(() => setIsLoading(false));
-	}, []);
+	}, [searchPhrase, isAlphabetSorting]);
 
 	return (
 		<div className={styles.app}>
-			{isLoading ? (
-				<div className={styles.loadingLabel}></div>
-			) : (
-				todos.map(({ id, userId, title, completed }) => (
-					<div key={id} className={styles.todo}>
-						<input
-							className={styles.checkbox}
-							type="checkbox"
-							checked={completed}
-							readOnly
+			<ControlPanel
+				onTodoAdd={onTodoAdd}
+				onSearch={setSearchPhrase}
+				onSorting={setIsAlphabetSorting}
+			/>
+			<div>
+				{isLoading ? (
+					<div className={styles.loadingLabel}></div>
+				) : (
+					todos.map(({ id, title, completed, isEditing = false }) => (
+						<Todo
+							key={id}
+							id={id}
+							title={title}
+							completed={completed}
+							isEditing={isEditing}
+							onTitleChange={(newTitle) =>
+								onTodoTitleChange(id, newTitle)
+							}
+							onCompletedChange={(newCompleted) =>
+								onTodoCompletedChange(id, newCompleted)
+							}
+							onSave={() => onTodoSave(id)}
+							onRemove={() => onTodoRemove(id)}
+							onEdit={() => onTodoEdit(id)}
 						/>
-						№: {id}. - {title}: Статус задачи:
-						{completed ? " Завершена" : " Надо завершить"}
-					</div>
-				))
-			)}
+					))
+				)}
+			</div>
 		</div>
 	);
 };
---
+++ src/components/control-panel/control-panel.js
@@ -0,0 +1,16 @@
+// Наш компонент управления панелью
+
+import styles from "./control-panel.module.css";
+
+import { Button } from "../button/button";
+import { Search, Sorting } from "./components";
+
+export const ControlPanel = ({ onTodoAdd, onSearch, onSorting }) => {
+	return (
+		<div className={styles.controlPanel}>
+			<Search onSearch={onSearch} />
+			<Sorting onSorting={onSorting} />
+			<Button onClick={onTodoAdd}>✚</Button>
+		</div>
+	);
+};
