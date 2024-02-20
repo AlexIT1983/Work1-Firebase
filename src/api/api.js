dae76a23923f169e6474f06417b26ba55078d2ec
Alex Antipov <alex.antipov@gmail.com>
2024-02-20 23:02:42 +0300

    Work1 variant3 FireBase

--- src/api/api.js
+++ src/api/api.js
@@ -1,41 +1,51 @@
 // Наши функции для приложения
 
-import { HTTP_METHOD } from "../constants";
-
-const fetchServer = (method, { id, ...payload } = {}) => {
-	// функция для обработки запроса
-	let url = `http://localhost:3005/todos`;
-	let options = {
-		method,
-		headers: { "Content-type": "application/json" },
-	};
-	if (method === HTTP_METHOD.GET) {
-		const { searchPhrase, isAlphabetSorting } = payload;
-		const sortingParams = isAlphabetSorting
-			? "_sort=title&_order=asc"
-			: "_sort=id&_order=desc";
-		url += `?${sortingParams}&title_like=${searchPhrase}`;
-	} else {
-		if (method !== HTTP_METHOD.POST) {
-			url += `/${id}`;
-		}
-		if (method !== HTTP_METHOD.DELETE) {
-			options.body = JSON.stringify(payload);
-		}
-	}
-
-	return fetch(url, options).then((jsonData) => jsonData.json());
-};
+import {
+	get,
+	orderByChild,
+	query,
+	ref,
+	push,
+	set,
+	remove,
+} from "firebase/database";
+import { db } from "../firebase";
 
 // функция добавления задачи
-export const createTodo = (newTodo) => fetchServer("POST", newTodo);
+export const createTodo = (newTodo) =>
+	push(ref(db, "todos"), newTodo).then(({ key }) => key);
 
 // функция чтения данных
-export const readTodos = (searchPhrase = "", isAlphabetSorting = false) =>
-	fetchServer("GET", { searchPhrase, isAlphabetSorting });
+export const readTodos = (searchPhrase = "", isAlphabetSorting = false) => {
+	const todosDbRef = ref(db, "todos");
+	const orderingField = isAlphabetSorting ? "title" : "id";
+
+	return get(query(todosDbRef, orderByChild(orderingField))).then(
+		(snapshot) => {
+			let loadedTodos = [];
+
+			snapshot.forEach((todoSnapshot) => {
+				const id = todoSnapshot.key;
+				const { title, completed } = todoSnapshot.val();
+				loadedTodos.push({ id, title, completed });
+			});
+			if (searchPhrase !== "") {
+				loadedTodos = loadedTodos.filter(
+					({ title }) =>
+						title
+							.toLowerCase()
+							.indexOf(searchPhrase.toLowerCase()) >= 0,
+				);
+			}
+
+			return isAlphabetSorting ? loadedTodos : loadedTodos.reverse();
+		},
+	);
+};
 
 // функция редактированя задачи
-export const updateTodo = (todoData) => fetchServer("PATCH", todoData);
+export const updateTodo = (todoData) =>
+	set(ref(db, `todos/${todoData.id}`), todoData);
 
 // функция удаления задачи
-export const deleteTodo = (todoId) => fetchServer("DELETE", { id: todoId });
+export const deleteTodo = (todoId) => remove(ref(db, `todos/${todoId}`));
